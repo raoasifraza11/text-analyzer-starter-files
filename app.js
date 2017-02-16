ace94cde19e4dd16146f798cca42c0947389ce43
raoasifraza11 <raoasifraza1@hotmail.com>
2017-02-16 16:07:04 +0500

    Added word count

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 // your code here!
+$('#text-analyse').submit(function (event) {
+    event.preventDefault();
+    var inputText = $(event.currentTarget).find('textarea[name="user-text"]').val();
+    console.log(inputText);
+    console.log(countWords(inputText));
+    displayResult(inputText.length)
+});
+
+function countWords(text){
+    for(var i = 0; i < text.length; i++){
+
+    }
+}
+
+function displayResult(lenght) {
+    $('dl').removeClass('hidden');
+    $('.js-word-count').text(lenght);
+}
